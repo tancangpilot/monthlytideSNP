7715a90839b6fcadea95c3aabae39f74dddd5b77
NP44 <hai.pt.pilot@gmail.com>
2026-04-02 19:31:53 +0700

    Add files via upload

---
+++ tab_window.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import os
+from utils.data_processor import process_and_style_df
+
+def render_window_tab(file_path, sheet_name, show_past, disclaimer_text):
+    st.markdown(disclaimer_text)
+    if os.path.exists(file_path):
+        try:
+            df = pd.read_excel(file_path, sheet_name=sheet_name)
+            styled_df = process_and_style_df(df, show_past_dates=show_past)
+            
+            st.dataframe(
+                styled_df, 
+                use_container_width=True, 
+                height=750, 
+                hide_index=True, 
+                column_config={
+                    "_dow": None, 
+                    "_actual_date": None, 
+                    "Date": st.column_config.TextColumn("Date", pinned=True) # <-- Cố định cột Date
+                }
+            )
+        except Exception as e:
+            st.error(f"Lỗi đọc sheet {sheet_name}: {e}")
+    else:
+        st.warning(f"Chưa có dữ liệu hiển thị. Không tìm thấy file '{file_path}'.")
